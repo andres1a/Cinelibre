a11059f77ea5a7f3796b2fd97e581e0d52e1370b
Andrés Solano <afslora@gmail.com>
2025-06-13 03:16:32 -0500

    Slider responsive

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-const arrows = document.querySelectorAll('.arrow');
-const movieLists = document.querySelectorAll('.movie-list');
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
 
 arrows.forEach((arrow, i) => {
-    const itemNumber = movieLists[i].querySelectorAll("img").length;
-    let clickCounter = 0;
-    arrow.addEventListener('click', () => {
-    // 1) obtenemos el estilo computado
-    const style = window.getComputedStyle(movieLists[i]);
-    // 2) extraemos la transform actual como DOMMatrix
-    const matrix = new DOMMatrixReadOnly(style.transform);
-    const currentX = matrix.m41;           // el desplazamiento X actual
-    const nextX    = currentX - 300;       
-
-    // 3) lo aplicamos de nuevo
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
     clickCounter++;
-    if(itemNumber - (4 + clickCounter) > 0){
-        movieLists[i].style.transform = `translateX(${nextX}px)`;
-    } else{
-       movieLists[i].style.transform = "translateX(0px)"; 
-       clickCounter = 0; // Reiniciar el contador si no hay más elementos
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
     }
-    
   });
 
+  console.log(Math.floor(window.innerWidth / 270));
+});
 
-  console.log(movieLists[i].querySelectorAll("img"))
+//TOGGLE
 
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
 
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
 });
